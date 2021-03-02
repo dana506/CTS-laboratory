dd28988ea0f9277ca56fbdce63b3c9fa96d323e1
dana <danamaria_buzatu@yahoo.com>
2021-03-02 15:54:04 +0200

    2nd commit

--- Seminar 2/src/ro/ase/csie/cts/g1094/seminar2/models/BankAccount.java
+++ Seminar 2/src/ro/ase/csie/cts/g1094/seminar2/models/BankAccount.java
@@ -1,5 +1,18 @@
 package ro.ase.csie.cts.g1094.seminar2.models;
 
-public class BankAccount {
+public abstract class BankAccount extends Account{
+	protected double balance;
+	protected final String iban; 
+	
+	public BankAccount(double balance, String iban) {
+		this.balance = balance;
+		this.iban = iban;
+	}
 
+	@Override
+	public double getBalance() {
+		return this.balance;
+	}
+	
+	
 }
