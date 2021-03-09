d4eae67c81761101173929115cec5fe226d7f22f
dana <danamaria_buzatu@yahoo.com>
2021-03-09 14:46:29 +0200

    before break time

---
+++ Seminar 3/src/ro/ase/csie/acs/g1094/refactor/phase1/Product.java
@@ -0,0 +1,34 @@
+package ro.ase.csie.acs.g1094.refactor.phase1;
+
+public class Product {
+
+	public static final int MAX_AGE_ACCOUNT = 10;
+	public static final float MAX_FIDELITY_DISCOUNT = 0.15f;
+
+	public float ComputePriceWithDiscount(ProductType productType, float price, int accountAge) {
+		float finalPrice = 0;
+		float fidelityDiscount = (accountAge > MAX_AGE_ACCOUNT) ? MAX_FIDELITY_DISCOUNT : (float) accountAge / 100;
+
+		switch (productType) {
+		case NEW:
+			finalPrice = price;
+			break;
+		case DISCOUNTED:
+			finalPrice = (price - (ProductType.DISCOUNTED.getDiscount() * price))
+					- fidelityDiscount * (price - (ProductType.DISCOUNTED.getDiscount() * price));
+			break;
+		case LIMITED_STOCK:
+			finalPrice = (price - (ProductType.LIMITED_STOCK.getDiscount() * price))
+					- fidelityDiscount * (price - (ProductType.LIMITED_STOCK.getDiscount() * price));
+			break;
+		case OLD:
+			finalPrice = (price - (ProductType.OLD.getDiscount() * price))
+					- fidelityDiscount * (price - (ProductType.OLD.getDiscount() * price));
+			break;
+		default:
+			throw new UnsupportedOperationException("The enum type is not covered");
+		}
+
+		return finalPrice;
+	}
+}
---
+++ Seminar 3/src/ro/ase/csie/acs/g1094/refactor/phase1/ProductType.java
@@ -0,0 +1,21 @@
+package ro.ase.csie.acs.g1094.refactor.phase1;
+
+public enum ProductType {
+
+	NEW(0),
+    DISCOUNTED(0.1f),
+    LIMITED_STOCK(0.25f),
+    OLD(0.35f);
+
+	
+	private final float discountValue;
+	
+	private ProductType (float discount) {
+		this.discountValue = discount;
+	}
+	
+	
+	public float getDiscount() {
+		return this.discountValue;
+	}
+}
